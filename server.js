fc4fa33b9fdc25f46f1dbd744b6e3731768d5d0c
ilavisharma <lavisharma796@gmail.com>
2019-08-02 17:18:22 +0530

    import schema in the server

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config({ path: 'variables.env' });
+const Recipe = require('./models/Recipe');
+const User = require('./models/User');
 
 // Connect to database
 mongoose
