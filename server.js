b6b5ce82d2579691387e7c4b5a59b3ad019197be
ilavisharma <lavisharma796@gmail.com>
2019-08-03 12:32:58 +0530

    configure cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 require('dotenv').config({ path: 'variables.env' });
 const Recipe = require('./models/Recipe');
 const User = require('./models/User');
@@ -23,6 +24,13 @@ mongoose
 
 // Initialise the app
 const app = express();
+
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+  })
+);
 
 // Create graphiql application
 app.use(
