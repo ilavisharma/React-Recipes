4087f9da5f4e40f355e8901f557e2748dadf6c74
ilavisharma <lavisharma796@gmail.com>
2019-08-03 12:24:03 +0530

    making the first query

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { Query } from 'react-apollo';
 import './App.css';
+import { GET_ALL_RECIPES } from '../queries';
 
-function App() {
-  return <div className="App">Home</div>;
-}
+const App = () => (
+  <div className="App">
+    <h1>Home</h1>
+    <Query query={GET_ALL_RECIPES}>
+      {({ data, loading, error }) => {
+        if (loading) return <div className="">Loading</div>;
+        if (error) return <div className="">Error</div>;
+        console.log(data);
+        return <p>Recipes</p>;
+      }}
+    </Query>
+  </div>
+);
 
 export default App;
