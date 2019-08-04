27dafb663bd4ac3af1a0b9b9dd7d778dd3ea5be8
ilavisharma <lavisharma796@gmail.com>
2019-08-04 16:38:25 +0530

    sign in op now working

--- client/src/components/Auth/Signin.js
+++ client/src/components/Auth/Signin.js
@@ -1,13 +1,89 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Mutation } from 'react-apollo';
+import { withRouter } from 'react-router-dom';
+import Error from '../Error';
+import { SIGNIN_USER } from '../../queries';
+
+const INITTIAL_STATE = {
+  username: '',
+  password: ''
+};
 
 class Signin extends Component {
-    render() {
-        return (
-            <div>
-                Signin
-            </div>
-        )
-    }
+  state = { ...INITTIAL_STATE };
+
+  clearState = () => {
+    this.setState({ ...INITTIAL_STATE });
+  };
+
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  validateForm = () => {
+    const { username, password } = this.state;
+
+    const isInvalid = !username || !password;
+    return isInvalid;
+  };
+
+  handleSubmit = (e, signinUser) => {
+    e.preventDefault();
+    signinUser().then(async ({ data }) => {
+      console.log(data);
+      localStorage.setItem('token', data.signinUser.token);
+      await this.props.refetch();
+      this.clearState();
+      this.props.history.push('/');
+    });
+  };
+
+  render() {
+    const { username, password } = this.state;
+    return (
+      <div className="App">
+        <h2 className="App">Signin</h2>
+        <Mutation mutation={SIGNIN_USER} variables={{ username, password }}>
+          {(signinUser, { data, loading, error }) => {
+            return (
+              <form
+                className="form"
+                onSubmit={e => this.handleSubmit(e, signinUser)}
+              >
+                <input
+                  type="text"
+                  name="username"
+                  placeholder="Username"
+                  value={username}
+                  onChange={this.handleChange}
+                />
+
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={this.handleChange}
+                />
+
+                <button
+                  type="submit"
+                  className="button-primary"
+                  disabled={loading || this.validateForm()}
+                >
+                  Submit
+                </button>
+                {error && <Error error={error} />}
+              </form>
+            );
+          }}
+        </Mutation>
+      </div>
+    );
+  }
 }
 
-export default Signin;
+export default withRouter(Signin);
