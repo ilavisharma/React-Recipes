b90081176963889ecae5086bc3323c44819631aa
ilavisharma <lavisharma796@gmail.com>
2019-08-04 10:56:47 +0530

    define pre for UserSchame

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const UserSchema = new Schema({
   username: {
@@ -22,6 +23,20 @@ const UserSchema = new Schema({
     type: [Schema.Types.ObjectId],
     ref: 'Recipe'
   }
+});
+
+UserSchema.pre('save', function(next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err);
+    bcrypt.hash(this.password, salt, (err, hash) => {
+      if (err) return next(err);
+      this.password = hash;
+      next();
+    });
+  });
 });
 
 module.exports = model('User', UserSchema);
