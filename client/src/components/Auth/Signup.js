a3e61c26974a900a35aed8332a6db2e4dbc1f5e5
ilavisharma <lavisharma796@gmail.com>
2019-08-04 13:28:29 +0530

    added form validation error component

--- client/src/components/Auth/Signup.js
+++ client/src/components/Auth/Signup.js
@@ -1,13 +1,103 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Mutation } from 'react-apollo';
+import { SIGNUP_USER } from '../../queries';
+import Error from '../Error';
+
+const INITTIAL_STATE = {
+  username: '',
+  email: '',
+  password: '',
+  passwordConfirmation: ''
+};
 
 class Signup extends Component {
-    render() {
-        return (
-            <div>
-                Signup
-            </div>
-        )
-    }
+  state = { ...INITTIAL_STATE };
+
+  clearState = () => {
+    this.setState({ ...INITTIAL_STATE });
+  };
+
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  validateForm = () => {
+    const { username, email, password, passwordConfirmation } = this.state;
+
+    const isInvalid =
+      !username || !email || !password || password !== passwordConfirmation;
+    return isInvalid;
+  };
+
+  handleSubmit = (e, signupUser) => {
+    e.preventDefault();
+    signupUser().then(data => {
+      console.log(data);
+      this.clearState();
+    });
+  };
+
+  render() {
+    const { username, email, password, passwordConfirmation } = this.state;
+    return (
+      <div className="App">
+        <h2 className="App">Signup</h2>
+        <Mutation
+          mutation={SIGNUP_USER}
+          variables={{ username, email, password }}
+        >
+          {(signupUser, { data, loading, error }) => {
+            return (
+              <form
+                className="form"
+                onSubmit={e => this.handleSubmit(e, signupUser)}
+              >
+                <input
+                  type="text"
+                  name="username"
+                  placeholder="Username"
+                  value={username}
+                  onChange={this.handleChange}
+                />
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Email Address"
+                  value={email}
+                  onChange={this.handleChange}
+                />
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={this.handleChange}
+                />
+                <input
+                  type="password"
+                  name="passwordConfirmation"
+                  placeholder="Confirm Password"
+                  value={passwordConfirmation}
+                  onChange={this.handleChange}
+                />
+                <button
+                  type="submit"
+                  className="button-primary"
+                  disabled={loading || this.validateForm()}
+                >
+                  Submit
+                </button>
+                {error && <Error error={error} />}
+              </form>
+            );
+          }}
+        </Mutation>
+      </div>
+    );
+  }
 }
 
 export default Signup;
