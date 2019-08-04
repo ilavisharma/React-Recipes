9b56ca61ad4a82200adf1f98a05380f572058b4c
ilavisharma <lavisharma796@gmail.com>
2019-08-04 16:37:58 +0530

    query added for current user user sign in

--- client/src/queries/index.js
+++ client/src/queries/index.js
@@ -1,28 +1,45 @@
-import {gql} from 'apollo-boost';
+import { gql } from 'apollo-boost';
 
 // RECIPE QUERIES
-export const GET_ALL_RECIPES= gql`
-    query {
-        getAllRecipes {
-            name
-            description
-            instructions
-            category
-            likes
-            createdDate
-        }
+export const GET_ALL_RECIPES = gql`
+  query {
+    getAllRecipes {
+      name
+      description
+      instructions
+      category
+      likes
+      createdDate
     }
-`
+  }
+`;
 
 // RECIPE MUTATIONS
 
 // USER QUERIES
+export const GET_CURRENT_USER = gql`
+  query {
+    getCurrentUser {
+      username
+      joinDate
+      email
+    }
+  }
+`;
 
 // USER MUTATIONS
-export const SIGNUP_USER= gql`
-    mutation($username: String!, $email: String!, $password: String!) {
-        signupUser(username: $username, email: $email, password: $password) {
-            token
-        }
+export const SIGNUP_USER = gql`
+  mutation($username: String!, $email: String!, $password: String!) {
+    signupUser(username: $username, email: $email, password: $password) {
+      token
     }
-`
+  }
+`;
+
+export const SIGNIN_USER = gql`
+  mutation($username: String!, $password: String!) {
+    signinUser(username: $username, password: $password) {
+      token
+    }
+  }
+`;
