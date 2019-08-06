338c4bd46d041ae04b3c50e711a27c20d0a3dcdc
ilavisharma <lavisharma796@gmail.com>
2019-08-06 18:27:58 +0530

    Navbar component with two local components NavbarAuth for authenticated users NavbarUnAuth for unauthenticated users

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = ({ session }) => (
+  <nav>
+    {session && session.getCurrentUser ? (
+      <NavbarAuth session={session} />
+    ) : (
+      <NavbarUnAuth />
+    )}
+  </nav>
+);
+
+// For authenticated users
+const NavbarAuth = ({ session }) => (
+  <>
+    <ul>
+      <li>
+        <NavLink to="/" exact>
+          Home
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to="/search">Search</NavLink>
+      </li>
+      <li>
+        <NavLink to="/recipe/add">Add Recipe</NavLink>
+      </li>
+      <li>
+        <NavLink to="/profile">Profile</NavLink>
+      </li>
+      <li>
+        <button>Signout</button>
+      </li>
+    </ul>
+    <h4>
+      Welcome, <strong>{session.getCurrentUser.username}</strong>
+    </h4>
+  </>
+);
+
+// For Unauthenticated users
+const NavbarUnAuth = () => (
+  <ul>
+    <li>
+      <NavLink to="/" exact>
+        Home
+      </NavLink>
+    </li>
+    <li>
+      <NavLink to="/search">Search</NavLink>
+    </li>
+    <li>
+      <NavLink to="/signin">Signin</NavLink>
+    </li>
+    <li>
+      <NavLink to="/signup">Signup</NavLink>
+    </li>
+  </ul>
+);
+
+export default Navbar;
