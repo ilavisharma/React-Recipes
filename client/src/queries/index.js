d11bd489cb4e5b8158e5e685a12f69fe801541f2
ilavisharma <lavisharma796@gmail.com>
2019-08-10 10:17:52 +0530

    add getRecipe query

--- client/src/queries/index.js
+++ client/src/queries/index.js
@@ -5,8 +5,21 @@ export const GET_ALL_RECIPES = gql`
   query {
     getAllRecipes {
       _id
-      name      
-      category      
+      name
+      category
+    }
+  }
+`;
+export const GET_RECIPE = gql`
+  query($_id: ID!) {
+    getRecipe(_id: $_id) {
+      _id
+      name
+      category
+      description
+      instructions
+      createdDate
+      likes
     }
   }
 `;
