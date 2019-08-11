19d23b2a108efba083a7fe08e326c9749e7aad3a
ilavisharma <lavisharma796@gmail.com>
2019-08-11 11:11:41 +0530

    search query for client

--- client/src/queries/index.js
+++ client/src/queries/index.js
@@ -25,6 +25,16 @@ export const GET_RECIPE = gql`
   }
 `;
 
+export const SEARCH_RECIPES = gql`
+  query($searchTerm: String) {
+    searchRecipes(searchTerm: $searchTerm) {
+      _id
+      name
+      likes
+    }
+  }
+`;
+
 // RECIPE MUTATIONS
 export const ADD_RECIPE = gql`
   mutation(
