31380a0da799fbc7e386d684223b06defaebcda6
ilavisharma <lavisharma796@gmail.com>
2019-08-16 11:49:51 +0530

    styling added header changed

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,13 +6,16 @@ import RecipeItem from './Recipe/RecipeItem';
 
 const App = () => (
   <div className="App">
-    <h1>Home</h1>
+    <h1 className="main-title">
+      Find Recipes You <strong>Love ♥</strong>
+    </h1>
     <Query query={GET_ALL_RECIPES}>
       {({ data, loading, error }) => {
         if (loading) return <div className="">Loading</div>;
         if (error) return <div className="">Error</div>;
+
         return (
-          <ul>
+          <ul className="cards">
             {data.getAllRecipes.map(recipe => (
               <RecipeItem key={recipe._id} {...recipe} />
             ))}
